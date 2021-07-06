c15e7f42dd8d2dd7729aa3f7ae5596de27119727
zhou <624061566@qq.com>
2021-07-06 17:16:56 +0800

    rust by example ch01 done

---
+++ 01-rust-by-example/ch01-hello/display.rs
@@ -0,0 +1,21 @@
+use std::fmt;
+#[derive(Debug)]
+struct Complex {
+    real: f32,
+    imag: f32,
+}
+
+impl fmt::Display for Complex {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} + {}i", self.real, self.imag)
+    }
+}
+
+fn main() {
+    let complex = Complex { real: 3.3 , imag: 7.2};
+    println!("Display: {}", complex);
+    println!("{:?}", complex)
+}
+
+
+
---
+++ 01-rust-by-example/ch01-hello/format.rs
@@ -0,0 +1,23 @@
+use std::fmt;
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+impl fmt::Display for Color {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // write!(f, "RGB ({0},{1},{2}) 0x{:X}{:X}{:X}", self.red, self.green, self.blue)
+        // better format
+        write!(f,"RGB ({0},{1},{2}) 0x{0:02X}{1:02X}{2:02X}", self.red, self.green, self.blue)
+    }
+}
+fn main() {
+    for color in [
+        Color { red: 128, green: 255, blue: 90 },
+        Color { red: 0, green: 3, blue: 254},
+        Color { red: 0, green: 0, blue: 0},
+    ].iter() {
+        println!("{}", *color);
+    }
+}
